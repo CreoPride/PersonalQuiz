4651a917e7608aa60e8f37884fd3394878ba0991
Konstantin Fomenkov <kolij2005@gmail.com>
2022-03-15 23:54:32 +0300

    release

--- PersonalQuiz/ViewControllers/ResultViewController.swift
+++ PersonalQuiz/ViewControllers/ResultViewController.swift
@@ -14,30 +14,23 @@ class ResultViewController: UIViewController {
     
     var answersChosen: [Answer]!
     private var animal: Animal?
-    
-    // 1. Передать массив с ответами на экран с результатами ok
-    // 2. Определить наиболее часто встречающийся тип животного
-    // 3. Отобразить результаты в соответствии с этим животным
-    // 4. Избавиться от кнопки возврата назад на экране результатов ok
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
         navigationItem.setHidesBackButton(true, animated: false)
-        animal = countAnimal()
+
+        animal = findAnimal()
         animalLabel.text = "ВЫ - \(animal?.rawValue ?? ".")!"
         definitionLabel.text = animal?.definition ?? ""
-
     }
-
-    private func countAnimal() -> Animal? {
+    
+    private func findAnimal() -> Animal? {
         var quantityOfAnimals: [Animal: Int] = [:]
 
         let animals = answersChosen.compactMap { $0.animal }
         for animal in animals {
             quantityOfAnimals[animal] = (quantityOfAnimals[animal] ?? 0) + 1
         }
-        let animal = quantityOfAnimals.sorted { $0.value > $1.value }.first?.key
-        return animal
+        return quantityOfAnimals.sorted { $0.value > $1.value }.first?.key
     }
 }
